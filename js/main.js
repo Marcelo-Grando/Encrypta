cb9885c5e31ffc0ff4877fc1cea6060c19be3642
Marcelo017 <86316237+Marcelo017@users.noreply.github.com>
2022-02-02 04:31:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,123 +1,95 @@
 
-* {
-    margin: 0;
-    padding: 0;
+const buttonUno = document.querySelector('#btn1');
+const buttonDos = document.querySelector('#btn2');
+
+
+
+function isLowerCase(str) {
+    return str == str.toLowerCase() && str != str.toUpperCase();
 }
 
-body {
-    background: rgba(204, 175, 147, 0.493);
-    margin: 0;
-    padding: 0;
+function detectarCaracterInvalido(texto) {
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] != ' ' && isLowerCase(texto[i]) === false) {
+            return false;
+        }
+    }
+    return true; 
 }
 
-header {
-    background: rgb(43, 44, 48);
-    text-align: left;
-    padding: 20px;
-    font-family: 'Oxanium', cursive;
-    color: honeydew;
+function copiar(texto) {
+    var text = texto.innerText;
+    navigator.clipboard.writeText(text);
 }
 
-.principal {
-    padding: 40px;
-    display: flex;
-    justify-content: space-evenly;
-    background: rgba(49, 44, 44, 0.836);
-}
-
-textarea {
-    width: 300px;
-    height: 200px;
-    padding: 10px;
-}
-
-.buttons {
-    text-align: center;
-}
-
-.buttons button {
-    width: 158px;
-    height: 30px;
-    background: rgba(228, 215, 215, 0.774);
-    border: none;
-    border-radius: 5px;
-    color: black;
-    font-family:monospace;
-}
-
-.buttons button:hover {
-    background: honeydew;
-    cursor: pointer;
-}
-
-
-.container div {
-    background: white;
-    color: black;
-    text-align: justify;
-    padding: 10px;
-    border: 1px solid black;
-    width: 300px;
-    height: 200px;
-    font-family:monospace;
-}
-
-.container button {
-    margin-top: 4px;
-    margin-left: 162px;
-    width: 158px;
-    height: 30px;
-    background: rgba(228, 215, 215, 0.774);
-    border: none;
-    border-radius: 5px;
-    color: black;
-    font-family:monospace;
-}
-
-.container button:hover {
-    background: honeydew;
-    cursor: pointer;
-}
-
-footer {
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-top: 15px;
-    padding-bottom: 10px;
-    background: rgba(14, 13, 13, 0.801);
-    color: honeydew;
-    font-family: 'Oxanium', cursive;
-}
-
-footer .content {
-    background: rgb(14, 13, 13);
-    padding-left: 5px;
-    padding-top: 5px;
-}
-
-.icons {
-    padding-top: 20px;
-    display: flex;
-    justify-content: space-evenly;
-    width: 100%;
-    height: 139px;
-    background: rgba(14, 13, 13, 0.801);
-}
-
-footer a {
-    margin-top: 45px;
-    padding: auto;
-    width: 50px;
-    height: 50px;
-    font-size: 50px;
-    background-color: transparent;
-    border: none;
-    color: white
+function remplazarElemento(container, elemento ) {
+    if (container.children) {
+        container.replaceChildren(elemento);
+    }
+    return container
 }
 
 
 
-footer a:hover {
-    color: rgb(172, 168, 168);
+function mostrarContenido(texto) {
+
+    const container = document.querySelector('.container');
+
+    const cajaDeTexto = document.createElement('div');
+    cajaDeTexto.innerHTML = texto;
+
+    let btn = document.createElement('button');
+    btn.innerText = 'copy';
+    
+    remplazarElemento(container, cajaDeTexto)
+
+    container.appendChild(cajaDeTexto)
+    container.appendChild(btn)
+
+    btn.onclick = () => copiar(cajaDeTexto)
+
+    return cajaDeTexto;
+}
+ 
+
+
+function encriptar() {
+    
+    var textoSinEncriptar = document.querySelector('textarea').value
+
+
+    if (textoSinEncriptar != '' && detectarCaracterInvalido(textoSinEncriptar)) {
+    
+        var textoEncriptado = textoSinEncriptar.replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('a', 'ai')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat')
+    
+        mostrarContenido(textoEncriptado);
+    } else {
+        if (textoSinEncriptar === '') return alert('ingrese un mensaje');
+        alert('Carácter invalido: no son validas letras mayúsculas y/o carácteres especiales')
+    }
+
+    document.querySelector('textarea').value = '';
+
 }
 
+function desencriptar() {
+
+    var textoEncriptado = document.querySelector('textarea').value
+    
+    var textoDesencriptado = textoEncriptado.replaceAll('ai', 'a')
+        .replaceAll('enter', 'e')
+        .replaceAll('imes', 'i')
+        .replaceAll('ober', 'o')
+        .replaceAll('ufat', 'u')
+    
+    mostrarContenido(textoDesencriptado);
+
+    document.querySelector('textarea').value = '';
+}
+    
+buttonUno.onclick = encriptar;
+buttonDos.onclick = desencriptar;
