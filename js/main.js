00c1d87a13867269727da00aca6b57f23e2ae24e
Marcelo017 <86316237+Marcelo017@users.noreply.github.com>
2022-02-02 04:29:45 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,103 +1,123 @@
 
-
-const buttonUno = document.querySelector('#btn1');
-const buttonDos = document.querySelector('#btn2');
-
-
-
-function isLowerCase(str) {
-    return str == str.toLowerCase() && str != str.toUpperCase();
+* {
+    margin: 0;
+    padding: 0;
 }
 
-function detectar(parametro) {
-    for (let i = 0; i < parametro.length; i++) {
-        if (parametro[i] === ' ' || isLowerCase(parametro) === true) {
-            return true;
-        }
-    }
-    return false; 
+body {
+    background: rgba(204, 175, 147, 0.493);
+    margin: 0;
+    padding: 0;
 }
 
-function copiar(texto) {
-    var text = texto.innerText;
-    navigator.clipboard.writeText(text);
+header {
+    background: rgb(43, 44, 48);
+    text-align: left;
+    padding: 20px;
+    font-family: 'Oxanium', cursive;
+    color: honeydew;
 }
 
-function mostrarContenido(parametro) {
-    
-    if (detectar(parametro)) {
-        const container = document.querySelector('.container');
-        let caja = document.createElement('div');
-        caja.innerHTML = parametro;
-        let btn = document.createElement('button');
-        btn.innerText = 'copy';
-        if (container.children) {
-            container.replaceChildren(caja)
-        }
-        container.appendChild(caja);
-        container.appendChild(btn);
-        btn.onclick = () => copiar(caja);
-    } else {
-        alert('Caracter invalido: el texto no puede contener mayusculas o caracteres especiales');
-    }
-    
+.principal {
+    padding: 40px;
+    display: flex;
+    justify-content: space-evenly;
+    background: rgba(49, 44, 44, 0.836);
 }
 
- 
-
-function encriptar(e) {
-    
-    var textoSinEncriptar = document.querySelector('textarea').value
-
-
-    if (textoSinEncriptar != '') {
-        let arreglo = [];
-
-    for (let i = 0; i < textoSinEncriptar.length; i++) arreglo.push(textoSinEncriptar[i])
-
-
-        for (let i = 0; i < arreglo.length; i++) {
-            if (arreglo[i] === 'a') {
-                arreglo[i] = 'ai'
-            }
-            if (arreglo[i] === 'e') {
-                arreglo[i] = 'enter'
-            }
-            if (arreglo[i] === 'i') {
-                arreglo[i] = 'imes'
-            }
-            if (arreglo[i] === 'u') {
-                arreglo[i] = 'ufat'
-            }
-            if (arreglo[i] === 'o') {
-                arreglo[i] = 'ober'
-            }
-        }
-    
-        var result = arreglo.join('')
-    
-        mostrarContenido(result);
-    } else {
-        alert('ingrese un mensaje');
-    }
-
-    document.querySelector('textarea').value = '';
-
+textarea {
+    width: 300px;
+    height: 200px;
+    padding: 10px;
 }
 
-function desencriptar() {
-    var textoEncriptado = document.querySelector('textarea').value
-    
-    var cambio = textoEncriptado.replaceAll('ai', 'a')
-        .replaceAll('enter', 'e')
-        .replaceAll('imes', 'i')
-        .replaceAll('ober', 'o')
-        .replaceAll('ufat', 'u')
-    
-    mostrarContenido(cambio);
-    document.querySelector('textarea').value = '';
+.buttons {
+    text-align: center;
 }
-    
-buttonUno.onclick = encriptar;
-buttonDos.onclick = desencriptar;
+
+.buttons button {
+    width: 158px;
+    height: 30px;
+    background: rgba(228, 215, 215, 0.774);
+    border: none;
+    border-radius: 5px;
+    color: black;
+    font-family:monospace;
+}
+
+.buttons button:hover {
+    background: honeydew;
+    cursor: pointer;
+}
+
+
+.container div {
+    background: white;
+    color: black;
+    text-align: justify;
+    padding: 10px;
+    border: 1px solid black;
+    width: 300px;
+    height: 200px;
+    font-family:monospace;
+}
+
+.container button {
+    margin-top: 4px;
+    margin-left: 162px;
+    width: 158px;
+    height: 30px;
+    background: rgba(228, 215, 215, 0.774);
+    border: none;
+    border-radius: 5px;
+    color: black;
+    font-family:monospace;
+}
+
+.container button:hover {
+    background: honeydew;
+    cursor: pointer;
+}
+
+footer {
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 15px;
+    padding-bottom: 10px;
+    background: rgba(14, 13, 13, 0.801);
+    color: honeydew;
+    font-family: 'Oxanium', cursive;
+}
+
+footer .content {
+    background: rgb(14, 13, 13);
+    padding-left: 5px;
+    padding-top: 5px;
+}
+
+.icons {
+    padding-top: 20px;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 139px;
+    background: rgba(14, 13, 13, 0.801);
+}
+
+footer a {
+    margin-top: 45px;
+    padding: auto;
+    width: 50px;
+    height: 50px;
+    font-size: 50px;
+    background-color: transparent;
+    border: none;
+    color: white
+}
+
+
+
+footer a:hover {
+    color: rgb(172, 168, 168);
+}
 
